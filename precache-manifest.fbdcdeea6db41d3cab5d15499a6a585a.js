4eb95edbdf8ec6c9dac2b99248919112c96264f0
MozyOk <mozy.okubo@gmail.com>
2019-11-16 00:25:30 +0200

    Updates

---
+++ precache-manifest.fbdcdeea6db41d3cab5d15499a6a585a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1381fcaf856b637297e8",
+    "url": "/static/css/main.77af515a.chunk.css"
+  },
+  {
+    "revision": "1381fcaf856b637297e8",
+    "url": "/static/js/main.766f6770.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "86c6e10ba302e5568798",
+    "url": "/static/css/2.71f17d59.chunk.css"
+  },
+  {
+    "revision": "86c6e10ba302e5568798",
+    "url": "/static/js/2.23f97018.chunk.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "dd22bf3932f56f5b8c32e18c598b7622",
+    "url": "/index.html"
+  }
+];
